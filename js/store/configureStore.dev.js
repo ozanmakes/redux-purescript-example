e76da82bc8f830a30bf1c3a077faf61234e2768b
Ozan Sener <ozan@ozansener.com>
2016-04-18 18:35:47 +0300

    Drop .purs extension from PureScript imports

--- js/store/configureStore.dev.js
+++ js/store/configureStore.dev.js
@@ -4,7 +4,7 @@ import createLogger from "redux-logger"
 import DevTools from "../containers/DevTools"
 import actionTransformer from "../store/actionTransformer"
 
-import { rootReducer } from "Reducers.purs"
+import { rootReducer } from "Reducers"
 
 const loggerMiddleware = createLogger({
   level: "info",
@@ -21,8 +21,8 @@ export default function configureStore (initialState) {
   const store = finalCreateStore(rootReducer, initialState)
   // Hot reload reducers (requires Webpack or Browserify HMR to be enabled)
   if (module.hot) {
-    module.hot.accept("Reducers.purs", () =>
-                      store.replaceReducer(require("Reducers.purs").default))
+    module.hot.accept("Reducers", () =>
+                      store.replaceReducer(require("Reducers").default))
   }
 
   return store
